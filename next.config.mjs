1e39aad5b7790c22f19b661ec1905653b41c8dfd
Labinot77 <156574729+Labinot77@users.noreply.github.com>
2024-10-13 13:23:15 +0300

    Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     appDir: true,
     swcPlugins: [["next-superjson-plugin", {}]],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["picsum.photos", "wallpapers.com", "lh3.googleusercontent.com", "avatars.githubusercontent.com", "res.cloudinary.com"],
   },
